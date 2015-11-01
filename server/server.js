d4a7075df9fb52e07713c6420727478844c7bc25
xopherdeep <heylisten@xtiv.net>
2015-10-31 23:38:34 -0700

    Dev vs Prod

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app     = express();
 var server  = require('http').Server(app);
-var site    = (process.env.npm_config_production) ? 'dist' : 'site';
+var site    = (typeof(process.env.NODE_ENV)!='undefined') ? 
+  'dist' : 'site';
+
+console.log("Server serving: "+site);
+
 
 app.use(express.static(site));
 app.use(express.static('server/public'));
