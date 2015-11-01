aa320a2d74b945a0f5dbd350dc4ce67c84b6b2c7
xopherdeep <heylisten@xtiv.net>
2015-10-31 21:53:58 -0700

    :lipstick: refactoring structure

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app     = express();
 var server  = require('http').Server(app);
+var site    = (process.env.npm_config_production) ? 'dist' : 'site';
 
-app.use(express.static('site'));
+app.use(express.static(site));
 app.use(express.static('server/public'));
 
 module.exports = server;
